f7bbdf2a63567abdd09ac1852b369e30f5033926
Rogers Wase <waserogers@gmail.com>
2025-04-25 23:36:51 +0100

    Exercises to improve my skills in programing

---
+++ script.js
@@ -0,0 +1,32 @@
+
+let fName = "Rogers";
+let lName = "Wase";
+let age = "41";
+let fColor = "royal blue";
+let hobby = "Football";
+let fClub = "Arsenal FC";
+
+console.log(`Hello my name is ${fName} ${lName}, I am ${age} years old and my fvorite color is ${fColor}. My hobby is ${hobby} and my favorite club is ${fClub}.`)
+
+
+
+let a = "Greetings";
+let b = 41;
+let c = true;
+
+console.log("The type of a is" + typeof (a) );
+console.log("The type of b is" + typeof b);
+console.log("The type of c is" + typeof c);
+
+
+
+let num1 = 20
+let num2 = 5
+
+console.log("The sum of " + num1 + " and " + num2 + " is " + (num1+num2))
+console.log("The subtraction of " + num1 + " and " + num2 + " is " + (num1 - num2));
+console.log("The multiplication of " + num1 + " and " + num2 + " is " + (num1 * num2));
+console.log("The division of " + num1 + " and " + num2 + " is " + (num1 / num2));
+
+
+
